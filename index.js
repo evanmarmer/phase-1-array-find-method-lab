36f554960c68429c2b721ec2fdcfb7927362129e
evanmarmer <evanmarmer@gmail.com>
2023-03-13 11:48:57 -0600

    find method lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record){
+console.log(record)
+const winObj = record.find(function(obj){
+    if (obj.result === "W"){
+        return obj
+    }
+}) 
+    if (winObj === undefined){
+        return undefined 
+    }
+return(winObj.year)
+}
